c20f6213a82bc8f192aa893bc2822acd1a8aa90e
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2025-08-12 14:11:14 -0300

    Criado rota para buscar por id, tratado rota post para inserir com titulo do body

--- server.js
+++ server.js
@@ -13,10 +13,28 @@ server.get('/courses', () => {
   return { courses }
 })
 
+server.get('/courses/:id', (request, reply) => {
+  // const { id } = request.params
+  const courseId = request.params.id // Da para fazer dos dois jeitos
+
+  const course = courses.find(course => course.id === courseId)
+
+  if (course) {
+    return { course }
+  }
+
+  return reply.status(404).send('Registro não encontrado!')
+})
+
 server.post('/courses', (request, reply) => {
   const courseId = crypto.randomUUID()
+  const courseTitle = request.body.title
 
-  courses.push({ id: courseId, title: 'Novo curso' })
+  if (!courseTitle) {
+    return reply.status(400).send({ message: 'Obrigatório informar um título!' })
+  }
+
+  courses.push({ id: courseId, title: courseTitle })
 
   return reply.status(201).send({ courseId })
 })
