1383fcc32e4f1472e37cde28760e3e2c8fab80e2
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2025-08-14 21:57:50 -0300

    No arquivo principal voltei as rotas como estavam para seguir com o curso e fiz uma cópia com as rotas que criei a mais

--- server-aula.ts
+++ server-aula.ts
@@ -1,106 +1,138 @@
+import { eq } from "drizzle-orm"
 import fastify from "fastify"
-import crypto from 'node:crypto'
-
+import { db } from "./src/database/client.ts"
+import { courses } from "./src/database/schema.ts"
 
 const server = fastify({
   logger: {
     transport: {
-      target: 'pino-pretty',
+      target: "pino-pretty",
       options: {
-        translateTime: 'HH:MM:ss Z',
-        ignore: 'pid,hosname',
+        translateTime: "HH:MM:ss Z",
+        ignore: "pid,hosname",
       },
     },
   },
 })
 
-const courses = [
-  { id: '1', title: 'Curso de Node.js' },
-  { id: '2', title: 'Curso de React' },
-  { id: '3', title: 'Curso de React Native' },
-]
+server.get("/courses", async (request, reply) => {
+  //const result = await db.select().from(courses) Se vazio, traz todos os campos
+  const result = await db
+    .select({
+      id: courses.id,
+      title: courses.title,
+    })
+    .from(courses)
 
-server.get('/courses', () => {
-  return { courses }
+  return reply.send({ courses: result })
 })
 
-server.get('/courses/:id', (request, reply) => { 
+server.get("/courses/:id", async (request, reply) => {
   type Params = {
     id: string
   }
-  
+
   const params = request.params as Params
-  const courseId = params.id 
+  const courseId = params.id
 
-  const course = courses.find(course => course.id === courseId)
+  const result = await db.select().from(courses).where(eq(courses.id, courseId))
 
-  if (course) {
-    return { course }
+  if (result.length > 0) {
+    return { course: result[0] }
   }
 
-  return reply.status(404).send('Registro não encontrado!')
+  return reply.status(404).send("Registro não encontrado!")
 })
 
-server.put('/courses/:id', (request, reply) => {
+server.post("/courses", async (request, reply) => {
+  type Body = {
+    title: string
+    description: string
+  }
+
+  const body = request.body as Body
+  const courseTitle = body.title
+  const courseDescription = body.description
+
+  if (!courseTitle) {
+    return reply
+      .status(400)
+      .send({ message: "Obrigatório informar um título!" })
+  }
+
+  const result = await db
+    .insert(courses)
+    .values({
+      title: courseTitle,
+      description: courseDescription,
+    })
+    .returning()
+
+  return reply.status(201).send({ courseId: result[0].id })
+})
+
+server.put("/courses/:id", async (request, reply) => {
   type Params = { id: string }
-  type Body = { title: string }
+  type Body = {
+    title: string
+    description: string
+  }
 
   const params = request.params as Params
   const body = request.body as Body
   const courseId = params.id
-  const title = body.title
+  const bodyTitle = body.title
+  const bodyDescription = body.description
 
+  const result = await db.select().from(courses).where(eq(courses.id, courseId))
 
-  const courseIndex = courses.findIndex(course => course.id === courseId)
-
-  if (courseIndex === -1) {
-    return reply.status(404).send({ message: 'Registro não encontrado!' })
+  if ((result.length = 0)) {
+    return reply.status(404).send({ message: "Registro não encontrado!" })
   }
 
-  if (!title) {
-    return reply.status(400).send({ message: 'Obrigatório informar um título!' })
+  if (!bodyTitle) {
+    return reply
+      .status(400)
+      .send({ message: "Obrigatório informar um título!" })
   }
 
-  courses[courseIndex].title = title
+  if (bodyDescription) {
+    await db
+      .update(courses)
+      .set({
+        title: bodyTitle,
+        description: bodyDescription,
+      })
+      .where(eq(courses.id, courseId))
+  } else {
+    await db
+      .update(courses)
+      .set({
+        title: bodyTitle,
+      })
+      .where(eq(courses.id, courseId))
+  }
 
-  return reply.send({ message: 'Curso atualizado com sucesso!' })
+  return reply.send({ message: "Curso atualizado com sucesso!" })
 })
 
-server.post('/courses', (request, reply) => {
-  type Body = {
-    title: string
-  }
-
-  const courseId = crypto.randomUUID()
-  const body = request.body as Body
-  const courseTitle = body.title
-
-  if (!courseTitle) {
-    return reply.status(400).send({ message: 'Obrigatório informar um título!' })
-  }
-
-  courses.push({ id: courseId, title: courseTitle })
-
-  return reply.status(201).send({ courseId })
-})
-
-server.delete('/courses/:id', (request, reply) => {
+server.delete("/courses/:id", async (request, reply) => {
   type Params = { id: String }
 
   const params = request.params as Params
   const courseId = params.id
 
-  const courseIndex = courses.findIndex(course => course.id === courseId)
+  const result = await db.select().from(courses).where(eq(courses.id, courseId))
 
-  if (courseIndex === -1) {
-    return reply.status(404).send({ message: 'Registro não encontrado!' })
+  if ((result.length = 0)) {
+    return reply.status(404).send({ message: "Registro não encontrado!" })
   }
 
-  courses.splice(courseIndex, 1)
+  await db.delete(courses).where(eq(courses.id, courseId))
 
-  return reply.send({ message: 'Curso excluído com sucesso!' })
+  return reply.send({ message: "Curso excluído com sucesso!" })
 })
 
 server.listen({ port: 3333 }).then(() => {
-  console.log('HTTP server running!')
+  console.log("HTTP server running!")
 })
