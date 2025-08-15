f402aa4e0612429d583aa823542c70d8b8d63de2
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2025-08-14 21:55:30 -0300

    Adicionado tratamento para rotas de update e delete (não foi feito no curso, mas fiz para praticar e como desafio pessoal)

--- server.ts
+++ server.ts
@@ -50,7 +50,6 @@ server.post("/courses", async (request, reply) => {
     description: string
   }
 
-  const courseId = crypto.randomUUID()
   const body = request.body as Body
   const courseTitle = body.title
   const courseDescription = body.description
@@ -70,6 +69,68 @@ server.post("/courses", async (request, reply) => {
     .returning()
 
   return reply.status(201).send({ courseId: result[0].id })
+})
+
+server.put("/courses/:id", async (request, reply) => {
+  type Params = { id: string }
+  type Body = {
+    title: string
+    description: string
+  }
+
+  const params = request.params as Params
+  const body = request.body as Body
+  const courseId = params.id
+  const bodyTitle = body.title
+  const bodyDescription = body.description
+
+  const result = await db.select().from(courses).where(eq(courses.id, courseId))
+
+  if ((result.length = 0)) {
+    return reply.status(404).send({ message: "Registro não encontrado!" })
+  }
+
+  if (!bodyTitle) {
+    return reply
+      .status(400)
+      .send({ message: "Obrigatório informar um título!" })
+  }
+
+  if (bodyDescription) {
+    await db
+      .update(courses)
+      .set({
+        title: bodyTitle,
+        description: bodyDescription,
+      })
+      .where(eq(courses.id, courseId))
+  } else {
+    await db
+      .update(courses)
+      .set({
+        title: bodyTitle,
+      })
+      .where(eq(courses.id, courseId))
+  }
+
+  return reply.send({ message: "Curso atualizado com sucesso!" })
+})
+
+server.delete("/courses/:id", async (request, reply) => {
+  type Params = { id: String }
+
+  const params = request.params as Params
+  const courseId = params.id
+
+  const result = await db.select().from(courses).where(eq(courses.id, courseId))
+
+  if ((result.length = 0)) {
+    return reply.status(404).send({ message: "Registro não encontrado!" })
+  }
+
+  await db.delete(courses).where(eq(courses.id, courseId))
+
+  return reply.send({ message: "Curso excluído com sucesso!" })
 })
 
 server.listen({ port: 3333 }).then(() => {
