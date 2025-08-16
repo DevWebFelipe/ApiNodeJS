486a55168a301503f14f6dcc64efa73ea492ecf1
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2025-08-15 22:28:08 -0300

    Adicionado ordenação, paginação e filtros

--- src/routes/get-courses.ts
+++ src/routes/get-courses.ts
@@ -1,6 +1,7 @@
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod"
 import { db } from "../database/client.ts"
 import { courses } from "../database/schema.ts"
+import { ilike, asc, SQL, and } from "drizzle-orm"
 import { z } from "zod"
 
 export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
@@ -10,6 +11,11 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
       schema: {
         tags: ["courses"],
         summary: "Listar todos os cursos",
+        querystring: z.object({
+          search: z.string().optional(),
+          orderBy: z.enum(["title", "id"]).optional().default("id"),
+          page: z.coerce.number().optional().default(1),
+        }),
         description: "Retorna um objeto com uma lista de objetos(courses)",
         response: {
           200: z
@@ -20,20 +26,36 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
                   title: z.string(),
                 })
               ),
+              total: z.number(),
             })
             .describe("Retorna um objeto com um array de objetos(courses)"),
         },
       },
     },
     async (request, reply) => {
-      const result = await db
-        .select({
-          id: courses.id,
-          title: courses.title,
-        })
-        .from(courses)
+      const { search, orderBy, page } = request.query
 
-      return reply.send({ courses: result })
+      const conditions: SQL[] = []
+
+      if (search) {
+        conditions.push(ilike(courses.title, `%${search}%`))
+      }
+
+      const [result, total] = await Promise.all([
+        db
+          .select({
+            id: courses.id,
+            title: courses.title,
+          })
+          .from(courses)
+          .where(and(...conditions))
+          .orderBy(asc(courses[orderBy]))
+          .offset((page - 1) * 2)
+          .limit(2),
+        db.$count(courses, and(...conditions)),
+      ])
+
+      return reply.send({ courses: result, total })
     }
   )
 }
