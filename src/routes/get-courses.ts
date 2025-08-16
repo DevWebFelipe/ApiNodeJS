f65bf6cd4f46b1b7654984857819185b7ed7fca3
Felipe Texeira <109564897+DevWebFelipe@users.noreply.github.com>
2025-08-15 23:02:01 -0300

    Adicionado campo para mostrar total de alunos matriculados em cada curso

--- src/routes/get-courses.ts
+++ src/routes/get-courses.ts
@@ -1,7 +1,7 @@
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod"
 import { db } from "../database/client.ts"
-import { courses } from "../database/schema.ts"
-import { ilike, asc, SQL, and } from "drizzle-orm"
+import { courses, enrollments } from "../database/schema.ts"
+import { ilike, asc, SQL, and, eq, count } from "drizzle-orm"
 import { z } from "zod"
 
 export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
@@ -24,6 +24,7 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
                 z.object({
                   id: z.uuid(),
                   title: z.string(),
+                  enrollments: z.number(),
                 })
               ),
               total: z.number(),
@@ -46,12 +47,15 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
           .select({
             id: courses.id,
             title: courses.title,
+            enrollments: count(enrollments.id),
           })
           .from(courses)
+          .leftJoin(enrollments, eq(enrollments.courseId, courses.id))
           .where(and(...conditions))
           .orderBy(asc(courses[orderBy]))
-          .offset((page - 1) * 2)
-          .limit(2),
+          .offset((page - 1) * 10)
+          .groupBy(courses.id)
+          .limit(10),
         db.$count(courses, and(...conditions)),
       ])
 
